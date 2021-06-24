c9f0ac37f80cef3063163fc86e65ebe2e0e82752
Alex L <alex@poorground.com>
2021-06-24 16:19:56 -0700

    fix nested arguments

--- index.js
+++ index.js
@@ -37,25 +37,48 @@ function parseFields (/* dynamic */) {
   return tree
 }
 
-function fieldTreeFromAST (asts, fragments, init) {
+function argumentTreeFromAST (asts, fragments, init) {
   init = init || {}
   fragments = fragments || {}
   asts = castArr(asts)
   return asts.reduce(function (tree, val) {
     var kind = val.kind
     var name = val.name && val.name.value
+    if (kind === 'Argument') {
+      tree[name] = tree[name] || {}
+      argumentTreeFromAST(val.value, fragments, tree[name])
+    } else if(kind == "ObjectField"){
+      tree[name] = tree[name] || {}
+      tree[name] = val.value.value
+    } else if(kind == "ObjectValue"){
+      argumentTreeFromAST(val.fields, fragments, tree)
+    }
+    return tree
+  }, init)
+}
+
+function fieldTreeFromAST (asts, fragments, init) {
+  init = init || {}
+  fragments = fragments || {}
+  asts = castArr(asts)
+  return asts.reduce(function (tree, val) {
+    var kind = val.kind 
+    var name = val.name && val.name.value
     var fragment
     if (kind === 'Field') {
       if(val.arguments){
         tree[name] = tree[name] || {}
         tree[name].arguments = tree[name].arguments || {}
-        val.arguments.forEach( arg => { tree[name].arguments[arg.name.value] = arg.value.value } )
+        if(val.arguments.length > 0){
+          argumentTreeFromAST(val.arguments, fragments, tree[name].arguments)
+        }
       }
       if (val.selectionSet) {
         tree[name] = tree[name] || {}
         tree[name].fields = tree[name].fields || {}
         fieldTreeFromAST(val.selectionSet.selections, fragments, tree[name].fields)
       } else {
+        tree[name] = tree[name] || {}
         tree[name].key = true
       }
     } else if (kind === 'FragmentSpread') {
